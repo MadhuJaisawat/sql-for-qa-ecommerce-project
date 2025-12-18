ff25348ae130c132233ddba148160a0c6f1526e8
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 07:55:27 +0530

    Create ecommerce_data.sql
    Added ecommerce data

---
+++ ecommerce_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (name, email, city) VALUES
+('Amit Sharma', 'amit@gmail.com', 'Delhi'),
+('Neha Verma', 'neha@gmail.com', 'Mumbai'),
+('Rahul Singh', 'rahul@gmail.com', 'Bangalore');
